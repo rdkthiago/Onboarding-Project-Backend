c06c17ee0784b8979ae1c471b3ffb7e6ce7fef3e
rdkthiago <rdkthiago19@gmail.com>
2026-03-27 19:23:52 -0300

    chore: initial backend setup with NestJS, Docker, PostgreSQL and TypeORM

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+  useFactory: () => {
+    console.log('ENV DEBUG:', {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      db: process.env.DB_NAME,
+    });
+
+    return {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5433', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    };
+  },
+}),
+    
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule {}
