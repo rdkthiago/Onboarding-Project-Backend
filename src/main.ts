71f99f05204c597849dfc661244dbaae5084f0d9
rdkthiago <rdkthiago19@gmail.com>
2026-03-29 12:04:25 -0300

    feat: add automatic API documentation with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Onboarding API')
+    .setDescription('Documentação dos endpoints do fluxo de cadastro')
+    .setVersion('1.0')
+    .addTag('registration')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('api/docs', app, document);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
